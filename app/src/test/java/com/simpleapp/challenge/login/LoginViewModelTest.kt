2d39c4fea39f20dbeacb95a224d36e4b3a080c40
Darush <darushfaqiri@gmail.com>
2021-09-28 22:01:47 +0800

    spec: add login view model test specifications

---
+++ app/src/test/java/com/simpleapp/challenge/login/LoginViewModelTest.kt
@@ -0,0 +1,25 @@
+package com.simpleapp.challenge.login
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class LoginViewModelTest {
+
+  @Test
+  fun givenUserClicksLogin_whenUsernameOrPasswordIsEmpty_thenShowRelevantErrorMessage() {
+
+  }
+
+  @Test
+  fun givenUserClicksLogin_whenUsernameOrPasswordIncorrect_thenShowError() {
+
+  }
+
+  @Test
+  fun givenUserClicksLogin_whenUsernameAndPasswordCorrect_thenNavigateToUserListScreen() {
+
+  }
+
+}
