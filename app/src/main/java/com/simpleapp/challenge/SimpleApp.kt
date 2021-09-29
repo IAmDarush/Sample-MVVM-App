729fb19f893c921ec9ffe4ee8a4fbaa1f36facc7
Darush <darushfaqiri@gmail.com>
2021-09-29 18:14:06 +0800

    feat: add Timber logging library

--- app/src/main/java/com/simpleapp/challenge/SimpleApp.kt
+++ app/src/main/java/com/simpleapp/challenge/SimpleApp.kt
@@ -2,6 +2,18 @@ package com.simpleapp.challenge
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class SimpleApp: Application()
+class SimpleApp: Application() {
+
+  override fun onCreate() {
+    super.onCreate()
+
+    if (BuildConfig.DEBUG) {
+      Timber.plant(Timber.DebugTree())
+    }
+
+  }
+
+}
