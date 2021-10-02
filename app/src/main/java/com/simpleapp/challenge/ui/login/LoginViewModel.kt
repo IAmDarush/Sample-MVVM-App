6d3f9dafa0c10b79282105de1b7d4200e4fafff1
Darush <darushfaqiri@gmail.com>
2021-10-03 00:18:03 +0800

    test: when user wants to login and the input fields are empty then show input validation error

--- app/src/main/java/com/simpleapp/challenge/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/simpleapp/challenge/ui/login/LoginViewModel.kt
@@ -1,9 +1,14 @@
 package com.simpleapp.challenge.ui.login
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.simpleapp.challenge.data.model.LoginModel
 import com.simpleapp.challenge.data.remote.ApiService
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,8 +16,46 @@ class LoginViewModel @Inject constructor(
   private val apiService: ApiService
 ) : ViewModel() {
 
-  fun login(model: LoginModel) {
-    TODO("Not yet implemented")
+  sealed class Event {
+    object FailedToValidateAllInputFields : Event()
+    data class FailedToLogin(val errorMessage: String) : Event()
+  }
+
+  private val eventChannel = Channel<Event>(Channel.BUFFERED)
+  val eventsFlow: Flow<Event> = eventChannel.receiveAsFlow()
+
+  fun login(userName: String, password: String, country: String) {
+    val model = LoginModel(userName, password, country)
+
+    when (validateInputs(model)) {
+      true  -> {
+        viewModelScope.launch {
+          val result = apiService.login(model)
+
+          when (result.isSuccess) {
+            true  -> {
+              TODO("Navigate to user list")
+            }
+            false -> {
+              val message = result.exceptionOrNull()?.message ?: "Login error"
+              eventChannel.send(Event.FailedToLogin(message))
+            }
+          }
+
+        }
+      }
+      false -> {
+        eventChannel.trySend(Event.FailedToValidateAllInputFields)
+      }
+    }
+  }
+
+  private fun validateInputs(model: LoginModel): Boolean {
+    return when {
+      model.userName.isEmpty() -> false
+      model.password.isEmpty() -> false
+      else                     -> true
+    }
   }
 
 }
--- app/src/test/java/com/simpleapp/challenge/ui/login/LoginViewModelTest.kt
+++ app/src/test/java/com/simpleapp/challenge/ui/login/LoginViewModelTest.kt
@@ -1,6 +1,16 @@
 package com.simpleapp.challenge.ui.login
 
+import androidx.lifecycle.viewModelScope
 import com.simpleapp.challenge.data.remote.ApiService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -10,19 +20,39 @@ import org.mockito.junit.MockitoJUnitRunner
 @RunWith(MockitoJUnitRunner::class)
 class LoginViewModelTest {
 
-  private lateinit var loginViewModel: LoginViewModel
+  @ExperimentalCoroutinesApi
+  val dispatcher = TestCoroutineDispatcher()
+
+  private lateinit var vm: LoginViewModel
 
   @Mock
   private lateinit var mockApiService: ApiService
 
+  @ExperimentalCoroutinesApi
   @Before
   fun setup() {
-    loginViewModel = LoginViewModel(mockApiService)
+    Dispatchers.setMain(dispatcher)
+    vm = LoginViewModel(mockApiService)
+  }
+
+  @ExperimentalCoroutinesApi
+  @After
+  fun tearDown() {
+    Dispatchers.resetMain()
   }
 
   @Test
   fun givenUserClicksLogin_whenUsernameOrPasswordIsEmpty_thenShowRelevantErrorMessage() {
+    val eventsList = mutableListOf<LoginViewModel.Event>()
+    vm.viewModelScope.launch {
+      vm.eventsFlow.collect {
+        eventsList.add(it)
+      }
+    }
 
+    vm.login("", "", "")
+    assertEquals(1, eventsList.size)
+    assertEquals(eventsList[0], LoginViewModel.Event.FailedToValidateAllInputFields)
   }
 
   @Test
