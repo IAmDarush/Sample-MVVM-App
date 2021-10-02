09022e8b308cfbe12930699c15ca69da0cd4c658
Darush <darushfaqiri@gmail.com>
2021-10-03 01:05:52 +0800

    test: when user wants to login and the username or password is incorrect then show the error message

--- app/src/test/java/com/simpleapp/challenge/ui/login/LoginViewModelTest.kt
+++ app/src/test/java/com/simpleapp/challenge/ui/login/LoginViewModelTest.kt
@@ -2,11 +2,13 @@ package com.simpleapp.challenge.ui.login
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.viewModelScope
+import com.simpleapp.challenge.data.model.LoginResponse
 import com.simpleapp.challenge.data.remote.ApiService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
@@ -17,7 +19,9 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito.doReturn
 import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.any
 
 @RunWith(MockitoJUnitRunner::class)
 class LoginViewModelTest {
@@ -56,14 +60,34 @@ class LoginViewModelTest {
     }
 
     vm.login("", "", "")
+
     assertEquals(1, eventsList.size)
     assertEquals(LoginViewModel.Event.FailedToValidateAllInputFields, eventsList[0])
     assertEquals(false, vm.showLoading.value)
   }
 
   @Test
-  fun givenUserClicksLogin_whenUsernameOrPasswordIncorrect_thenShowError() {
+  fun givenUserClicksLogin_whenUsernameOrPasswordIncorrect_thenShowError(): Unit = runBlocking {
+    val eventsList = mutableListOf<LoginViewModel.Event>()
+    vm.viewModelScope.launch {
+      vm.eventsFlow.collect {
+        eventsList.add(it)
+      }
+    }
 
+    val userName = "user_name"
+    val password = "pass_word"
+    val country = "abc"
+    val mockResponse =
+      LoginResponse(isSuccess = false, errorMessage = "Wrong password", errorCode = 400, token = "")
+
+    doReturn(mockResponse).`when`(mockApiService).login(any())
+
+    vm.login(userName = userName, password = password, country = country)
+
+    assertEquals(1, eventsList.size)
+    assertEquals(LoginViewModel.Event.FailedToLogin(mockResponse.errorMessage), eventsList[0])
+    assertEquals(false, vm.showLoading.value)
   }
 
   @Test
