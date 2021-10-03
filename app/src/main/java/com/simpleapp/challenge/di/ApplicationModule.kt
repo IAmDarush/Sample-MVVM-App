26cd64ec88dc11814b0611df72338ba9eac4e6e7
Darush <darushfaqiri@gmail.com>
2021-10-03 14:13:20 +0800

    feat: create a manager class for shared preferences

--- app/src/main/java/com/simpleapp/challenge/di/ApplicationModule.kt
+++ app/src/main/java/com/simpleapp/challenge/di/ApplicationModule.kt
@@ -1,10 +1,13 @@
 package com.simpleapp.challenge.di
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.util.Log
 import com.simpleapp.challenge.BuildConfig
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import timber.log.Timber
 import javax.inject.Singleton
@@ -20,6 +23,12 @@ object ApplicationModule {
       override fun isLoggable(tag: String?, priority: Int) =
         BuildConfig.DEBUG || priority >= Log.INFO
     }.apply { Timber.plant(this) }
+  }
+
+  @Provides
+  @Singleton
+  fun provideSharedPreferences(@ApplicationContext context: Context): SharedPreferences {
+    return context.getSharedPreferences("SimpleAppPreferences", Context.MODE_PRIVATE)
   }
 
 }
