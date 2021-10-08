9e143fae46c024046b2aa6b1174f45fdb2acdc4b
Darush <darushfaqiri@gmail.com>
2021-10-08 13:02:43 +0800

    test: given the app is opened, when the user is logged in then navigate to user list screen

--- app/src/main/java/com/simpleapp/challenge/ui/main/MainViewModel.kt
+++ app/src/main/java/com/simpleapp/challenge/ui/main/MainViewModel.kt
@@ -1,19 +1,38 @@
 package com.simpleapp.challenge.ui.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.simpleapp.challenge.data.repository.AuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel: ViewModel() {
+class MainViewModel @Inject constructor(private val authRepository: AuthRepository) : ViewModel() {
 
   sealed class Event {
-
+    object NavigateToLogin : Event()
   }
 
   private val eventChannel = Channel<Event>(Channel.BUFFERED)
   val eventsFlow: Flow<Event> = eventChannel.receiveAsFlow()
+
+  init {
+
+    viewModelScope.launch {
+      when (authRepository.isUserLoggedIn()) {
+        false -> {
+          eventChannel.send(Event.NavigateToLogin)
+        }
+        true  -> {
+          TODO("navigate to user list")
+        }
+      }
+    }
+
+  }
 
 }
--- app/src/test/java/com/simpleapp/challenge/ui/main/MainViewModelTest.kt
+++ app/src/test/java/com/simpleapp/challenge/ui/main/MainViewModelTest.kt
@@ -2,18 +2,23 @@ package com.simpleapp.challenge.ui.main
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.viewModelScope
+import com.simpleapp.challenge.data.repository.AuthRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
@@ -25,6 +30,9 @@ class MainViewModelTest {
   @get:Rule
   val rule = InstantTaskExecutorRule()
 
+  @Mock
+  private lateinit var mockAuthRepository: AuthRepository
+
   private lateinit var vm: MainViewModel
   private val eventsList = mutableListOf<MainViewModel.Event>()
 
@@ -32,12 +40,6 @@ class MainViewModelTest {
   @Before
   fun setup() {
     Dispatchers.setMain(dispatcher)
-    vm = MainViewModel()
-    vm.viewModelScope.launch {
-      vm.eventsFlow.collect {
-        eventsList.add(it)
-      }
-    }
   }
 
   @ExperimentalCoroutinesApi
@@ -48,8 +50,18 @@ class MainViewModelTest {
   }
 
   @Test
-  fun givenTheAppIsOpened_whenTheUserIsLoggedIn_thenNavigateToUserListScreen() {
+  fun givenTheAppIsOpened_whenTheUserIsLoggedIn_thenNavigateToUserListScreen(): Unit = runBlocking {
+    Mockito.`when`(mockAuthRepository.isUserLoggedIn()).thenReturn(false)
 
+    vm = MainViewModel(mockAuthRepository)
+    vm.viewModelScope.launch {
+      vm.eventsFlow.collect {
+        eventsList.add(it)
+      }
+    }
+
+    assertEquals(eventsList.size, 1)
+    assertEquals(eventsList[0], MainViewModel.Event.NavigateToLogin)
   }
 
   @Test
